1127bb1b4944a3b562736a7381c1b761e10ae56c
eduardcotmrf <eduard.cot@marfeel.com>
2019-06-12 10:23:05 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 git push -f git@github.com:eduardcotmrf/eduardcotmrf.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:eduardcotmrf/my-vuepress.git master:gh-pages
+#git push -f git@github.com:eduardcotmrf/my-vuepress.git master:gh-pages
 
 cd -
