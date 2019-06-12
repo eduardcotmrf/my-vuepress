1eded41cd3ee0006df643eaf5471b3585a0cc4d8
eduardcotmrf <eduard.cot@marfeel.com>
2019-06-12 09:44:32 +0200

    remove

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,6 +10,7 @@ module.exports = {
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
   description: description,
+  base: '/my-vuepress/',
 
   /**
    * Extra tags to be injected to the page HTML `<head>`
